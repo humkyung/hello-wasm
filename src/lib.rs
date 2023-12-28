c0b2e1f7449492991ef9cf2fd60b14d8236b663c
humkyung <humkyung@atools.co.kr>
2023-12-28 23:16:29 +0900

    initial upload

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn add(a: u32, b: u32) -> u32
+{
+    a + b
+}
